03ac99be8f2719727b21239e62f8565393c3184f
Liam Grace <liamgrace.896@gmail.com>
2017-06-27 14:07:48 +0100

    Another update

--- packages/airline-network/lib/airline.js
+++ packages/airline-network/lib/airline.js
@@ -26,11 +26,16 @@ function onInspectPart(inspectPart) {
 function onReplacePart(replacePart) {
     var factory = getFactory();
     var part = inspectPart.part;
+    var newPart = inspectPart.newPart;
+    var plane = part.plane;
+
+    var partAr
 
     return getAssetRegistry('org.team3.airline.Part')
         .then(function(ar) {
+            partAr = ar;
             var serviceLog = factory.newResource('org.team3.airline', 'ServiceLog');
-            serviceLog.type = inspectPart.serviceType;
+            serviceLog.type = 'REPLACED';
 
             if (!part.serviceHistory) {
                 part.serviceHistory = [];
@@ -39,6 +44,22 @@ function onReplacePart(replacePart) {
             part.serviceHistory.push(serviceLog);
 
             return ar.update(part);
+        })
+        .then(function() {
+            var serviceLog = factory.newResource('org.team3.airline', 'ServiceLog');
+            serviceLog.type = 'REPLACEMENT';
+
+            if (!newPart.serviceHistory) {
+                newPart.serviceHistory = [];
+            }
+
+            return partAr.update(newPart);
+        })
+        .then(function() {
+            return getAssetRegistry('org.team3.airline.Plane');
+        })
+        .then(function(ar) {
+            
         });
 }
 
